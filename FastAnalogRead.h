25f2edb10516d018f314f5ea7ae5e71cba2f0222
Johan Vandegriff <johan.vandegriff@gmail.com>
2017-08-04 22:48:23 -0400

    Added FastAnalogRead.h

---
+++ FastAnalogRead.h
@@ -0,0 +1,14 @@
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+//call this from the setup() function to speed up analogRead
+void EnableFastAnalogRead() {
+  // set prescale to 16
+  sbi(ADCSRA,ADPS2);
+  cbi(ADCSRA,ADPS1);
+  cbi(ADCSRA,ADPS0);
+}
